4a90f0124e4ed0bb0f0df087620d8d8866fac6cc
Audrey LY <a.ratha.ly@gmail.com>
2017-10-17 22:37:15 +0200

    afficherStats ne run pas ?

--- test/mediatheque/GenreTest.java
+++ test/mediatheque/GenreTest.java
@@ -6,19 +6,25 @@ import org.junit.Test;
 
 public class GenreTest {
 	
-	Genre g;
+	Genre g, g1, g2;
 	
 	@Test
 	public void constructeurTest() {
 		g = new Genre("Horror");
+		g1 = new Genre ("Documentary");
 		assertTrue(g.getNom().equals("Horror"));
+		assertTrue(g1.getNom().equals("Documentary"));
 	}
 	
 	@Test
 	public void emprunterTest() {
 		g = new Genre("KPOP");
+		g2 = new Genre("Hip Hop");
 		g.emprunter();
+		g2.emprunter();
+		g2.emprunter();
 		assertEquals(g.getNbEmprunts(),12);
+		assertEquals(g2.getNbEmprunts(),14);
 	}
 	
 	@Test
@@ -26,6 +32,12 @@ public class GenreTest {
 		g = new Genre("Animation");
 		g.modifier("Anime");
 		assertTrue(g.getNom().equals("Anime"));	
+	}
+	
+	public void afficherstatTest () {
+		g = new Genre("Thriller");
+		g.emprunter();
+		g.afficherStatistiques(); // ??
 	}
 
 }

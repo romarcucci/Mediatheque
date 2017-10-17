97c87bb5d2fe5794f5a3e94132432582d25d7607
Audrey LY <a.ratha.ly@gmail.com>
2017-10-17 22:49:53 +0200

    no message

---
+++ test/mediatheque/LettreRappelTest.java
@@ -0,0 +1,24 @@
+package mediatheque;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import mediatheque.client.Client;
+
+public class LettreRappelTest {
+
+	LettreRappel r;
+	Date d;
+    FicheEmprunt f;
+	
+	@Test
+	public void lettrerappelTest() {
+		d = new Date();
+		//f = new FicheEmprunt(new Mediatheque("Mediatheque Louis Aragon"), new Client("Charlie","Hunnam"), null);
+		//r = new LettreRappel (d,f);
+	}
+
+}

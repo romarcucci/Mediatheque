439667aa6da2e845937fedfcf8c2df8cc94234b0
Audrey LY <a.ratha.ly@gmail.com>
2017-10-20 13:53:51 +0200

    Rapport

---
+++ test/mediatheque/document/DocumentTest.java
@@ -0,0 +1,14 @@
+package mediatheque.document;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DocumentTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

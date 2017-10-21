8fb1b9b7a03bc4e1f1a99f25e7bebf99f8bf661e
Audrey LY <a.ratha.ly@gmail.com>
2017-10-21 15:27:48 +0200

    modif categorie client

--- test/mediatheque/client/CategorieClientTest.java
+++ test/mediatheque/client/CategorieClientTest.java
@@ -6,68 +6,72 @@ import org.junit.Test;
 
 public class CategorieClientTest {
 
+	CategorieClient cc = null;
+	
 	@Test
-	public void nameConstructor() {
-		CategorieClient cc = new CategorieClient("marcucci");
-		assertTrue(cc.getNom().equals("marcucci"));
-		assertTrue(cc.getNbEmpruntMax() == 0);
-		assertTrue(cc.getCotisation() == 0);
-		assertTrue(cc.getCoefDuree() == 0);
-		assertTrue(cc.getCoefTarif() == 0);
-		assertTrue(!cc.getCodeReducUtilise());
+	public void nomConstructeurTest() {
+		cc = new CategorieClient("Subscriber");
+		assertNotNull(cc);
+		assertTrue(cc.getNom().equals("Subscriber"));
+		assertEquals(0, cc.getNbEmpruntMax());
+		assertEquals(0,cc.getCotisation(),0.001);
+		assertEquals(0,cc.getCoefDuree(),0.001);
+		assertEquals(0,cc.getCoefTarif(),0.001);
+		assertEquals(false,cc.getCodeReducUtilise());
 	}
 
 	@Test
-	public void fullConstructor() {
-		CategorieClient cc = new CategorieClient("marcucci", 10, 10, 10,10, true);
-		assertTrue(cc.getNom().equals("marcucci"));
-		assertTrue(cc.getNbEmpruntMax() == 10);
-		assertTrue(cc.getCotisation() == 10);
-		assertTrue(cc.getCoefDuree() == 10);
-		assertTrue(cc.getCoefTarif() == 10);
-		assertTrue(cc.getCodeReducUtilise());
+	public void constructeurTest() {
+		CategorieClient cc = new CategorieClient("Subscriber", 10, 10, 10,10, true);
+		assertNotNull(cc);
+		assertTrue(cc.getNom().equals("Subscriber"));
+		assertEquals(10, cc.getNbEmpruntMax());
+		assertEquals(10,cc.getCotisation(),0.001);
+		assertEquals(10,cc.getCoefDuree(),0.001);
+		assertEquals(10,cc.getCoefTarif(),0.001);
+		assertEquals(true,cc.getCodeReducUtilise());
 	}
 	
 	@Test
-	public void modifNom(){
-		CategorieClient cc = new CategorieClient("marcucci", 10, 10, 10,10, true);
-		cc.modifierNom("iccucram");
-		assertTrue(cc.getNom().equals("iccucram"));
+	public void modifierNomTest(){
+		CategorieClient cc = new CategorieClient("Subscriber", 10, 10, 10,10, true);
+		cc.modifierNom("Normal Rate");
+		assertTrue(cc.getNom().equals("Normal Rate"));
 	}
 	
 	@Test
-	public void modifMax(){
-		CategorieClient cc = new CategorieClient("marcucci", 10, 10, 10,10, true);
+	public void modifierMaxTest(){
+		CategorieClient cc = new CategorieClient("Subscriber", 10, 10, 10,10, true);
 		cc.modifierMax(20);
-		assertTrue(cc.getNbEmpruntMax() == 20);
+		assertEquals(20,cc.getNbEmpruntMax());
 	}
 	
 	@Test
-	public void modifCotisation(){
-		CategorieClient cc = new CategorieClient("marcucci", 10, 10, 10,10, true);
+	public void modifierCotisationTestFail(){
+		CategorieClient cc = new CategorieClient("Subscriber", 10, 10, 10,10, true);
 		cc.modifierCotisation(20);
-		assertTrue(cc.getCotisation() == 20);
+		assertEquals(20,cc.getCotisation(),0.001);
 	}
 	
 	@Test
 	public void modifCoefDuree(){
-		CategorieClient cc = new CategorieClient("marcucci", 10, 10, 10,10, true);
+		CategorieClient cc = new CategorieClient("Subscriber", 10, 10, 10,10, true);
 		cc.modifierCoefDuree(20);
-		assertTrue(cc.getCoefDuree() == 20);
+		assertEquals(20,cc.getCoefDuree(),0.001);
 	}
 	
 	@Test
 	public void modifCoefTarif(){
-		CategorieClient cc = new CategorieClient("marcucci", 10, 10, 10,10, true);
+		CategorieClient cc = new CategorieClient("Subscriber", 10, 10, 10,10, true);
 		cc.modifierCoefTarif(20);
-		assertTrue(cc.getCoefTarif() == 20);
+		assertEquals(20,cc.getCoefTarif(),0.001);
 	}
 	
 	@Test
 	public void modifCodeReducActif(){
 		CategorieClient cc = new CategorieClient("marcucci", 10, 10, 10,10, true);
 		cc.modifierCodeReducActif(false);
-		assertTrue(!cc.getCodeReducUtilise());		
+		assertEquals(false,cc.getCodeReducUtilise());		
 	}
 }
 

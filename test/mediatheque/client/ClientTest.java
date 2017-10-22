257a8f3548f4f1fd4e482dc441fc526ef793b9c1
Romain Marcucci <romain.marcucci@gmail.com>
2017-10-22 22:52:06 +0200

    decommenter

--- test/mediatheque/client/ClientTest.java
+++ test/mediatheque/client/ClientTest.java
@@ -1,19 +1,24 @@
 package mediatheque.client;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import java.lang.reflect.Method;
 import java.util.Calendar;
 import java.util.Date;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import mediatheque.FicheEmprunt;
+import mediatheque.Genre;
+import mediatheque.Localisation;
+import mediatheque.Mediatheque;
 import mediatheque.OperationImpossible;
+import mediatheque.document.Audio;
+import util.InvariantBroken;
 
 public class ClientTest {
-	
+
 	CategorieClient cc = null;
 	Client c = null;
 
@@ -37,7 +42,7 @@ public class ClientTest {
 		assertTrue(client.getNom().equals("Nom"));
 		assertTrue(client.getPrenom().equals("Prenom"));
 		assertTrue(client.getAdresse().equals("Adresse"));
-		assertEquals(cc,client.getCategorie());
+		assertEquals(cc, client.getCategorie());
 	}
 
 	@Test(expected = OperationImpossible.class)
@@ -53,8 +58,8 @@ public class ClientTest {
 		assertTrue(client.getNom().equals("Nom"));
 		assertTrue(client.getPrenom().equals("Prenom"));
 		assertTrue(client.getAdresse().equals("Adresse"));
-		assertEquals(cc,client.getCategorie());
-		assertEquals(0,client.getReduc());
+		assertEquals(cc, client.getCategorie());
+		assertEquals(0, client.getReduc());
 	}
 
 	@Test(expected = OperationImpossible.class)
@@ -63,18 +68,18 @@ public class ClientTest {
 	}
 
 	@Test
-	public void initAttrTest(){ // --> private not be tested
-		
+	public void initAttrTest() { // --> private not be tested
+
 	}
 
 	@Test
 	public void pasEmpruntsEnCours() {
-		assertEquals(false, c.aDesEmpruntsEnCours());	
+		assertEquals(false, c.aDesEmpruntsEnCours());
 	}
 
 	@Test
 	public void empruntsEnCours() throws OperationImpossible {
-		assertEquals(false,c.aDesEmpruntsEnCours());
+		assertEquals(false, c.aDesEmpruntsEnCours());
 		c.emprunter();
 		assertTrue(c.aDesEmpruntsEnCours());
 	}
@@ -87,18 +92,19 @@ public class ClientTest {
 	}
 
 	@Test
-	public void nePeutEmprunter1() throws OperationImpossible { //a atteint son max
+	public void nePeutEmprunter1() throws OperationImpossible { // a atteint son
+																// max
 		c.getCategorie().modifierMax(1);
 		c.emprunter();
-		assertEquals(false,c.peutEmprunter());
+		assertEquals(false, c.peutEmprunter());
 	}
-	
+
 	@Test
-	public void nePeutEmprunter2() throws OperationImpossible { //retard 
+	public void nePeutEmprunter2() throws OperationImpossible { // retard
 		c.getCategorie().modifierMax(1);
 		c.emprunter();
-		assertEquals(c.getNbEmpruntsEnRetard(),c.getNbEmpruntsEnCours());
-		assertEquals(false,c.peutEmprunter());
+		assertEquals(c.getNbEmpruntsEnRetard(), c.getNbEmpruntsEnCours());
+		assertEquals(false, c.peutEmprunter());
 	}
 
 	@Test
@@ -111,29 +117,30 @@ public class ClientTest {
 		assertEquals(empruntsEnCours + 1, c.getNbEmpruntsEnCours());
 	}
 
-	// @Test
-	// public void emprunterFiche() throws OperationImpossible, InvariantBroken{
-	// int empruntsEffectues = c.getNbEmpruntsEffectues();
-	// int empruntsEnCours = c.getNbEmpruntsEnCours();
-	// c.getCategorie().modifierMax(10);
-	// Mediatheque m = new Mediatheque("mediatheque");
-	// Localisation l = new Localisation("A", "05");
-	// Genre g = new Genre("genre");
-	// Audio a = new Audio("code", l, "audio_doc", "auteur", "2017", g,
-	// "classification");
-	// FicheEmprunt fiche = new FicheEmprunt(m, c, a);
-	// c.emprunter(fiche);
-	// assertTrue(c.getNbEmpruntsEffectues() == empruntsEffectues + 1);
-	// assertTrue(c.getNbEmpruntsEnCours() == empruntsEnCours + 1);
-	// }
+	@Test
+	public void emprunterFiche() throws OperationImpossible, InvariantBroken {
+		int empruntsEffectues = c.getNbEmpruntsEffectues();
+		int empruntsEnCours = c.getNbEmpruntsEnCours();
+		c.getCategorie().modifierMax(10);
+		Mediatheque m = new Mediatheque("mediatheque");
+		Localisation l = new Localisation("A", "05");
+		Genre g = new Genre("genre");
+		Audio a = new Audio("code", l, "audio_doc", "auteur", "2017", g, "classification");
+		FicheEmprunt fiche = new FicheEmprunt(m, c, a);
+		c.emprunter(fiche);
+		assertTrue(c.getNbEmpruntsEffectues() == empruntsEffectues + 1);
+		assertTrue(c.getNbEmpruntsEnCours() == empruntsEnCours + 1);
+	}
 
 	@Test
-	public void marquer() throws OperationImpossible { //we changed to return value of getNbEmpruntsEnRetard
+	public void marquer() throws OperationImpossible { // we changed to return
+														// value of
+														// getNbEmpruntsEnRetard
 		c.getCategorie().modifierMax(10);
-		int nbRetard = c.getNbEmpruntsEnRetard(); 
+		int nbRetard = c.getNbEmpruntsEnRetard();
 		c.emprunter();
 		c.marquer();
-		assertEquals(nbRetard+1,c.getNbEmpruntsEnRetard());
+		assertEquals(nbRetard + 1, c.getNbEmpruntsEnRetard());
 	}
 
 	@Test(expected = OperationImpossible.class)
@@ -142,60 +149,60 @@ public class ClientTest {
 		c.marquer();
 	}
 
-	// @Test
-	// public void restituerFiche(){
-	// CategorieClient cc = new CategorieClient("marcucci");
-	// Client c = new Client("marcucci", "romain", "paris", cc);
-	// int empruntsEffectues = c.getNbEmpruntsEffectues();
-	// c.getCategorie().modifierMax(10);
-	// Mediatheque m = new Mediatheque("mediatheque");
-	// Localisation l = new Localisation("A", "05");
-	// Genre g = new Genre("genre");
-	// Audio a = new Audio("code", l, "audio_doc", "auteur", "2017", g,
-	// "classification");
-	// FicheEmprunt fiche = new FicheEmprunt(m, c, a);
-	// c.emprunter(fiche);
-	// c.restituer(fiche);
-	// assertTrue(c.getNbEmpruntsEnCours() == empruntsEnCours - 1);
-	// }
-	//
-	// @Test(expected = OperationImpossible)
-	// public void restituerFicheFail(){
-	// CategorieClient cc = new CategorieClient("marcucci");
-	// Client c = new Client("marcucci", "romain", "paris", cc);
-	// int empruntsEffectues = c.getNbEmpruntsEffectues();
-	// c.getCategorie().modifierMax(10);
-	// Mediatheque m = new Mediatheque("mediatheque");
-	// Localisation l = new Localisation("A", "05");
-	// Genre g = new Genre("genre");
-	// Audio a = new Audio("code", l, "audio_doc", "auteur", "2017", g,
-	// "classification");
-	// FicheEmprunt fiche = new FicheEmprunt(m, c, a);
-	// c.emprunter(fiche);
-	// Mediatheque m1 = new Mediatheque("error");
-	// FicheEmprunt fiche = new FicheEmprunt(m1, c, a);
-	// c.restituer(fiche);
-	// assertTrue(c.getNbEmpruntsEnCours() == empruntsEnCours - 1);
-	// }
+	 @Test
+	 public void restituerFiche(){
+	 CategorieClient cc = new CategorieClient("marcucci");
+	 Client c = new Client("marcucci", "romain", "paris", cc);
+	 int empruntsEffectues = c.getNbEmpruntsEffectues();
+	 c.getCategorie().modifierMax(10);
+	 Mediatheque m = new Mediatheque("mediatheque");
+	 Localisation l = new Localisation("A", "05");
+	 Genre g = new Genre("genre");
+	 Audio a = new Audio("code", l, "audio_doc", "auteur", "2017", g,
+	 "classification");
+	 FicheEmprunt fiche = new FicheEmprunt(m, c, a);
+	 c.emprunter(fiche);
+	 c.restituer(fiche);
+	 assertTrue(c.getNbEmpruntsEnCours() == empruntsEnCours - 1);
+	 }
+
+	 @Test(expected = OperationImpossible)
+	 public void restituerFicheFail(){
+	 CategorieClient cc = new CategorieClient("marcucci");
+	 Client c = new Client("marcucci", "romain", "paris", cc);
+	 int empruntsEffectues = c.getNbEmpruntsEffectues();
+	 c.getCategorie().modifierMax(10);
+	 Mediatheque m = new Mediatheque("mediatheque");
+	 Localisation l = new Localisation("A", "05");
+	 Genre g = new Genre("genre");
+	 Audio a = new Audio("code", l, "audio_doc", "auteur", "2017", g,
+	 "classification");
+	 FicheEmprunt fiche = new FicheEmprunt(m, c, a);
+	 c.emprunter(fiche);
+	 Mediatheque m1 = new Mediatheque("error");
+	 FicheEmprunt fiche = new FicheEmprunt(m1, c, a);
+	 c.restituer(fiche);
+	 assertTrue(c.getNbEmpruntsEnCours() == empruntsEnCours - 1);
+	 }
 
 	@Test
 	public void restituerBool() throws OperationImpossible {
 		c.getCategorie().modifierMax(10);
 		c.emprunter();
 		c.restituer(false);
-		assertEquals(0,c.getNbEmpruntsEnCours());
+		assertEquals(0, c.getNbEmpruntsEnCours());
 	}
 
-	// @Test
-	// public void restituerBoolRetard() throws OperationImpossible{
-	// CategorieClient cc = new CategorieClient("marcucci");
-	// Client c = new Client("marcucci", "romain", "paris", cc);
-	// c.getCategorie().modifierMax(10);
-	// c.emprunter();
-	// c.restituer(true);
-	// assertTrue(c.getNbEmpruntsEnCours() == 0);
-	// assertTrue(c.getNbEmpruntsEnRetard() == 0);
-	// }
+	 @Test
+	 public void restituerBoolRetard() throws OperationImpossible{
+	 CategorieClient cc = new CategorieClient("marcucci");
+	 Client c = new Client("marcucci", "romain", "paris", cc);
+	 c.getCategorie().modifierMax(10);
+	 c.emprunter();
+	 c.restituer(true);
+	 assertTrue(c.getNbEmpruntsEnCours() == 0);
+	 assertTrue(c.getNbEmpruntsEnRetard() == 0);
+	 }
 
 	@Test(expected = OperationImpossible.class)
 	public void restituerBoolFail() throws OperationImpossible {
@@ -228,7 +235,7 @@ public class ClientTest {
 
 	@Test
 	public void sommeDue() throws OperationImpossible {
-		assertEquals(10 * cc.getCoefTarif(),c.sommeDue(10),0.001);
+		assertEquals(10 * cc.getCoefTarif(), c.sommeDue(10), 0.001);
 	}
-	
+
 }
